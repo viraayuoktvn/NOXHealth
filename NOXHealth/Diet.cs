ff457ef080552fa7ff1b77bf2f3b24cab60dc024
viraayuoktvn <79135457+viraayuoktvn@users.noreply.github.com>
2021-04-29 23:45:48 +0700

    Update Diet.cs

--- NOXHealth/Diet.cs
+++ NOXHealth/Diet.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,13 +11,23 @@ namespace NOXHealth
         public int carbo { get; set; }
         public int protein { get; set; }
         public int vitamin { get; set; }
+        public int fat { get; set; }
 
         public Diet(DateTime time)
         {
             this.time = time;
         }
 
+        public Diet()
+        {
+        }
+
         public override void showRecommendation()
+        {
+
+        }
+
+        public void showNutritions()
         {
 
         }
